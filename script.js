46bb88005443db9e62328cd54936c92751d3d033
Creative-mindset21 <tobiawotoye03@gmail.com>
2026-06-02 20:16:54 +0100

    Finalised the Rock Paper Scissor application
    Created a game that collects the user input five times and display the scores of the player and the computer

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
+let humanScore = 0;
+let computerScore = 0;
 
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function getHumanChoice() {
+  const userInput = prompt(
+    "Choose a value: 'Rock', 'Paper', 'Scissors'",
+  ).toLowerCase();
+  return userInput;
+}
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log(`It's a tie! You both chose ${humanChoice}`);
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    humanScore++;
+    console.log(
+      `You win! You chose ${humanChoice} while computer chose ${computerChoice}`,
+    );
+  } else {
+    computerScore++;
+    console.log(
+      `You lost! You chose ${humanChoice} while computer chose ${computerChoice}`,
+    );
+  }
+}
+
+function playGame() {
+  const playerScoreEL = document.getElementById("player-score");
+  const computerScoreEL = document.getElementById("computer-score");
+  for (let i = 0; i < 5; i++) {
+    let human = getHumanChoice();
+    let computer = getComputerChoice();
+    playRound(human, computer);
+  }
+  playerScoreEL.textContent = humanScore;
+  computerScoreEL.textContent = computerScore;
+}
+
+playGame();
